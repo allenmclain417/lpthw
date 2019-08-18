efbfbf1e4b6669301d51278c4f7cded4b13ce744
Allen McLain <allenmclain417@gmail.com>
2019-08-18 19:30:37 -0400

    Add files via upload
    Completed Exercises

---
+++ Ex_05/ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Rodney A. McLain'
+my_age = 41
+my_height = 74 # inches
+my_weight = 277 # lbs
+my_eyes = 'Hazel'
+my_teeth = 'White'
+my_hair = 'Blonde'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "That's heavier than is healthy 0_0"
+print "He's got %s eyes and %s hair." % (my_eyes,my_hair)
+print "His teeth are usually %s depending on if he had coffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight,
+my_age + my_height + my_weight)
